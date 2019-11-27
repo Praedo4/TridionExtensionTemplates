0030c942c01bedf98f236f01529917f2c79f7c5e
Nikita Jefymov <nikita.jefymov@trivident.com>
2019-11-27 10:42:24 +0100

    Renamed EventSystemExtension class so that it is NOT named the same as the project. This used to cause problems if Project name contains dots (Example.EventSystem cannot be the class name, but can be a project name)

---
+++ TemplateSources/EventSystem/EventSystem.cs
@@ -0,0 +1,28 @@
+﻿using Tridion.ContentManager.ContentManagement;
+using Tridion.ContentManager.Extensibility;
+using Tridion.ContentManager.Extensibility.Events;
+
+namespace EventSystemExtension
+{
+    [TcmExtension("EventSystemExtension")]
+    public class EventSystem : TcmExtension
+    {
+        public EventSystem()
+        {
+            Subscribe();
+        }
+
+        private void RenamingHandler(RepositoryLocalObject item, SaveEventArgs args, EventPhases phase)
+        {
+            // perform custom action when saving
+            // in our case adding the suffix to the item title
+            item.Title = $"{item.Title}_SampleExtensionSuffix";
+        }
+
+        public void Subscribe()
+        {
+            // subscribe for saving event
+            EventSystem.Subscribe<RepositoryLocalObject, SaveEventArgs>(RenamingHandler, EventPhases.Initiated);
+        }
+    }
+}
